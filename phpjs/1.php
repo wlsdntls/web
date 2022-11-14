b5de6ef3bff5ef3b316ccc88a04b0f05c392c62d
JINWOO\E470 <ifason@naver.com>
2022-11-14 23:54:32 +0900

    php 및 자바스크립트 첫 연습

---
+++ phpjs/1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>php</h1>
+    <?php
+      echo 10+10;
+     ?>
+     <h1>JavaScript</h1>
+     <script>
+       document.write(10+10);
+     </script>
+  </body>
+</html>
---
+++ phpjs/2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Javascript</h1>
+    <script>
+    document.write("10"+"10");
+    </script>
+    <h2>php</h2>
+    <?php
+    echo "10"."10";
+     ?>
+  </body>
+</html>
