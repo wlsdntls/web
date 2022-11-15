842b492bc6ce4c3318457878594f7e6cb1e3c81f
JINWOO\E470 <ifason@naver.com>
2022-11-15 23:46:54 +0900

    javascript php 변수설정

---
+++ phpjs/4.php
@@ -0,0 +1,21 @@
+<?php
+$name = "진우";
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>javascript</h1>
+    <script type="text/javascript">
+      name= "wlsdntls";
+      document.write("안녕하세요."+name);
+    </script>
+    <h1>php</h1>
+    <?php
+    echo "안녕하세요." .$name;
+    ?>
+  </body>
+</html>
