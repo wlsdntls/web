fca345d953c1cc41d32d6a7e8806a882f6d383a1
JINWOO\E470 <ifason@naver.com>
2022-11-15 23:54:52 +0900

    php 변수 및 조건문 로그인기능 만들기

---
+++ phpjs/8.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    $password = $_GET["password"];
+    if($password == "1111"){
+      echo "hi wlsdn";
+    }
+    else{
+      echo "who?";
+    }
+     ?>
+  </body>
+</html>
