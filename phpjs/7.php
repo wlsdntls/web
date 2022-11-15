c971e63312b8da19cd7d8fe1d8c05cf670d8b511
JINWOO\E470 <ifason@naver.com>
2022-11-15 23:54:39 +0900

    JavaScript 변수 및 조건문 로그인기능 만들기

---
+++ phpjs/7.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <script type="text/javascript">
+      password = prompt("비밀번호");
+      if(password == 1111) {
+        document.write("안녕하세요.진우");
+      }
+      else {
+        document.write("who?");
+      }
+    </script>
+  </body>
+</html>
