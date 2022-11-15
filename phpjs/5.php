4a9f71c9f896612900a5ca50dbb6d86b5761caa9
JINWOO\E470 <ifason@naver.com>
2022-11-15 23:47:45 +0900

    javascript 및 php 비교사용 (true false)

---
+++ phpjs/5.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>javascript</h1>
+    <script type="text/javascript">
+    document.write(1==1);
+    </script>
+    <h1>php</h1>
+    <?php
+    var_dump(1==1);
+     ?>
+  </body>
+</html>
