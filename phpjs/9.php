a51db349fb32a11b27763064db8ae548ff0f101c
JINWOO\E470 <ifason@naver.com>
2022-11-15 23:55:10 +0900

    JavaScript 및 php 반복문

---
+++ phpjs/9.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>javascript</h1>
+    <ul>
+    <script type="text/javascript">
+    i = 0;
+      while(i < 10){
+        document.write("<li>hello World</li>");
+        i = i + 1;
+      }
+    </script>
+  </ul>
+    <h1>php</h1>
+    <ul>
+    <?php
+    $i = 0;
+    while($i < 10){
+      echo "<li>hello world</li>";
+      $i = $i +1;
+    }
+    ?>
+  </ul>
+  </body>
+</html>
