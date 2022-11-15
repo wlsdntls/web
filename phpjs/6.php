09ce6a5df5085535a88f8dfbbc8ff93f4aee923d
JINWOO\E470 <ifason@naver.com>
2022-11-15 23:53:44 +0900

    javascript 및 php 조건문배우기

---
+++ phpjs/6.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>javascript</h1>
+    <script type="text/javascript">
+    result = (1==2);
+    if(result){
+      document.write("참");
+    }
+    else {
+      document.write("거짓");
+    }
+    </script>
+    <h1>php</h1>
+    <?php
+    $result = (2==2);
+    if($result){
+      echo "참";
+    }
+    else{
+      echo "거짓";
+    }
+  ?>
+  </body>
+</html>
