4eb3e31ac6f052ac6b9e491c7cc497a7859e6fc0
JINWOO\E470 <ifason@naver.com>
2022-11-16 23:11:48 +0900

    JS 및 PHP 함수 학습 - return에 관해.

---
+++ phpjs/13.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>JavaScript</h1>
+    <script type="text/javascript">
+      function a(input){
+        return input+1;
+      }
+      document.write(a(6));
+      prompt(a(6));
+    </script>
+
+    <h1>php</h1>
+    <?php
+      function a($input){
+        return $input + 1;
+      }
+      echo a(8);
+     ?>
+  </body>
+</html>
