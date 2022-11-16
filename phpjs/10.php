1089ad947c7fcd04512af24832fcdaaaccff1e45
JINWOO\E470 <ifason@naver.com>
2022-11-16 23:08:50 +0900

    JS 및 PHP 배열 사용

---
+++ phpjs/10.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>JavaScript</h1>
+    <script type="text/javascript">
+      list = new Array("one", "two", "three");
+      document.write(list[2]);
+      document.write(list.length);
+      </script>
+
+    <h1>php</h1>
+    <?php
+      $list = array("one", "two", "three");
+      echo $list[2];
+      echo count($list);
+     ?>
+  </body>
+</html>
