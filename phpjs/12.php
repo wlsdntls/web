ee5c13266d07393cd504edba3a92dd264a761295
JINWOO\E470 <ifason@naver.com>
2022-11-16 23:11:00 +0900

    JS 및 PHP 함수 첫 학습

---
+++ phpjs/12.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>JavaScript</h1>
+    <script type="text/javascript">
+      function a(){
+        document.write("Hello JS Function");
+      }
+      a();
+    </script>
+
+    <h1>php</h1>
+    <?php
+      function a(){
+        echo "hello PHP Function";
+      }
+      a();
+     ?>
+  </body>
+</html>
